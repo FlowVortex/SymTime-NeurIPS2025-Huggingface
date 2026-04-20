64acea099f47c18d7d3e8ef91e586ecb3e991d05
wwhenxuan <wwhenxuan@gmail.com>
2026-04-20 09:45:49 +0800

    whenxuan: update the model

---
+++ model.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from einops import rearrange, repeat
+from transformers.modeling_utils import PreTrainedModel
+
+
+class SymTimeModel(PreTrainedModel):
+    pass
