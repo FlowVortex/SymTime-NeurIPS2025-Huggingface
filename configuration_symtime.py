db69c6116a9025b856c3184bfd76885e81517bbf
wwhenxuan <wwhenxuan@gmail.com>
2026-04-20 10:04:15 +0800

    whenxuan: update the config of symtime

--- configuration_symtime.py
+++ configuration_symtime.py
@@ -6,17 +6,59 @@ from transformers.configuration_utils import PretrainedConfig
 
 
 @dataclass
-claas SymTimeConfig(PretrainedConfig):
-    context_window: 256
-    patch_len: 16
-    stride: 8
-    time_layers: 6
-    d_model: 512
-    n_heads: 8
-    d_ff: 2048
-    norm: BatchNorm
-    attn_dropout: 0.1
-    dropout: 0.1
-    act: gelu
-    pre_norm: False
-    hidden_size: 768
+class SymTimeConfig(PretrainedConfig):
+    """
+    Time series encoder configuration for SymTime Model.
+
+    Parameters
+    -----------
+    patch_size
+        The size of the patch to be used for the input data.
+    num_layers
+        The number of layers to be used for the encoder.
+    d_model
+        The dimension of the model.
+    d_ff
+        The dimension of the feedforward network.
+    num_heads
+        The number of heads to be used for the attention mechanism.
+    norm
+        The normalization to be used for the encoder.
+    attn_dropout
+        The dropout rate to be used for the attention mechanism.
+    dropout
+        The dropout rate to be used for the encoder.
+    act
+        The activation function to be used for the encoder.
+    pre_norm
+        Whether to use pre-norm for the encoder.
+    """
+
+    model_type = "time_series_transformer"
+
+    def __init__(
+        self,
+        patch_size: int = 16,
+        num_layers: int = 3,
+        d_model: int = 128,
+        d_ff: int = 256,
+        num_heads: int = 16,
+        norm: str = "BatchNorm",
+        attn_dropout: float = 0.0,
+        dropout: float = 0.0,
+        act: str = "gelu",
+        pre_norm: bool = True,
+        **kwargs,
+    ):
+        self.patch_size = patch_size
+        self.num_layers = num_layers
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_ff = d_ff
+        self.norm = norm
+        self.attn_dropout = attn_dropout
+        self.dropout = dropout
+        self.act = act
+        self.pre_norm = pre_norm
+
+        super().__init__(**kwargs)
